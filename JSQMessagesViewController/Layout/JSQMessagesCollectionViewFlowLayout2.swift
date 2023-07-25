3bbeca6e4d972c9c89e9317d3722d76e207c15c4
5HT <maxim@synrc.com>
2023-07-25 18:13:36 +0300

    init

---
+++ JSQMessagesViewController/Layout/JSQMessagesCollectionViewFlowLayout2.swift
@@ -0,0 +1,8 @@
+import UIKit
+
+public let kJSQMessagesCollectionViewCellLabelHeightDefault: CGFloat = 20
+public let kJSQMessagesCollectionViewAvatarSizeDefault: CGFloat = 30
+
+open class JSQMessagesCollectionViewFlowLayout2: UICollectionViewFlowLayout {
+
+}
